2e7691478f5efa7cc180e2100faecfd9a921cee2
Franklyn <franklyn1998@live.com>
2021-10-05 15:22:05 -0300

    Projeto criado

---
+++ src/application/Program.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.util.Locale;
+import java.util.Set;
+import java.util.TreeSet;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+
+		Set<Product> set = new TreeSet<>();
+
+		set.add(new Product("TV", 900.0));
+		set.add(new Product("Notebook", 1200.0));
+		set.add(new Product("Tablet", 400.0));
+
+		for (Product p : set) {
+			System.out.println(p);
+		}
+	}
+
+}
